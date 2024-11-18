29fc69327f1ef82a002cdef60524e157dd5b0a02
Esteban López <elopez@extraspace.com>
2024-11-18 01:37:04 -0600

    Ejs added, index working +-

--- app.js
+++ app.js
@@ -9,31 +9,34 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+// Configuración del motor de vistas
+app.set('views', path.join(__dirname, 'views')); // Ruta de las vistas
+app.engine('html', require('ejs').renderFile);  // Usar EJS para archivos .html
+app.set('view engine', 'html');                 // Motor de vistas como HTML
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Manejador de errores 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Configurar variables locales de error
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Renderizar página de error
   res.status(err.status || 500);
   res.render('error');
 });
